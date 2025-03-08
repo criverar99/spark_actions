19f4e7bab1e770bcab94f879dbb5a55372c3ecc8
criverar99 <c.riverar99@gmail.com>
2025-03-07 19:52:29 -0600

    Added hello world

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+from pyspark.sql import SparkSession
+
+#Create a spark session
+spark = SparkSession.builder.appName("HelloWorld").getOrCreate()
+
+#Use sql() to write a raw SQL query
+df = spark.sql("SELECT 'Hello World! AS Hello'")
+
+# Print the dataframe
+df.show()
+df.write.mode("overwrite").json("results")
